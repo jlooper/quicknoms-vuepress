682aba8da98a5625c129152df68a2158d4615599
Jen Looper <jen.looper@gmail.com>
2019-12-01 16:52:35 -0500

    more prep for videos

---
+++ docs/.vuepress/theme/layouts/VideoLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="markdown-body font-serif text-lg rounded-lg shadow-lg">
+      <RecipesList :list="$site.pages" />
+    </div>
+  </div>
+</template>
+<script>
+import VideosList from "@theme/components/VideosList.vue";
+
+export default {
+  components: {
+    VideosList
+  }
+};
+</script>
