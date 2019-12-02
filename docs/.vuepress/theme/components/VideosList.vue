06040e0e6ec2daf32e690d06997c00b473ddd874
Jen Looper <jen.looper@gmail.com>
2019-12-01 20:44:44 -0500

    moving files around

--- docs/.vuepress/theme/components/VideosList.vue
+++ docs/.vuepress/theme/components/VideosList.vue
@@ -4,7 +4,7 @@
     <div class="flex content-between flex-wrap">
       <div
         v-for="(item, index) in filteredList"
-        class="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3"
+        class="w-full p-2 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2"
       >
         <div class="rounded h-full overflow-hidden shadow-lg">
           <iframe
